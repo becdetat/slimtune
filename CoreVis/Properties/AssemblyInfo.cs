c6f07783bbdee05392750417b2c2ec29c5b21c07
promit.roy <promit.roy@766506a8-2ead-11de-ba44-d58fe8a9ce33>
2010-09-27 21:43:02 +0000

    Creating common assembly info file.

--- CoreVis/Properties/AssemblyInfo.cs
+++ CoreVis/Properties/AssemblyInfo.cs
@@ -29,29 +29,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SlimTuneUI.CoreVis")]
 [assembly: AssemblyDescription("Core visualization plugins for SlimTune Profiler.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("SlimDX Group")]
-[assembly: AssemblyProduct("SlimTune Profiler")]
-[assembly: AssemblyCopyright("Copyright © SlimDX Group 2010")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("7e16ee73-5662-4d6e-8cc0-6735e25728da")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.0.0")]
-[assembly: AssemblyFileVersion("0.3.0.0")]
