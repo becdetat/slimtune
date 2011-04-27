84dc29ac211d69ab03d54f32efcbe70db6bede1b
promit.roy <promit.roy@766506a8-2ead-11de-ba44-d58fe8a9ce33>
2011-04-27 01:54:00 +0000

    Added missing visualizer toolbar code.

---
+++ SlimTuneUI/VisualizerToolbar.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace SlimTuneUI
+{
+	partial class VisualizerToolbar
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if(disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VisualizerToolbar));
+			this.ToolStrip = new System.Windows.Forms.ToolStrip();
+			this.RefreshButton = new System.Windows.Forms.ToolStripButton();
+			this.ActiveSnapshotLabel = new System.Windows.Forms.ToolStripLabel();
+			this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+			this.ToolStrip.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// ToolStrip
+			// 
+			this.ToolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.RefreshButton,
+            this.ActiveSnapshotLabel,
+            this.toolStripSeparator1});
+			this.ToolStrip.Location = new System.Drawing.Point(0, 0);
+			this.ToolStrip.Name = "ToolStrip";
+			this.ToolStrip.Size = new System.Drawing.Size(827, 25);
+			this.ToolStrip.TabIndex = 0;
+			// 
+			// RefreshButton
+			// 
+			this.RefreshButton.Image = ((System.Drawing.Image) (resources.GetObject("RefreshButton.Image")));
+			this.RefreshButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.RefreshButton.Name = "RefreshButton";
+			this.RefreshButton.Size = new System.Drawing.Size(66, 22);
+			this.RefreshButton.Text = "Refresh";
+			this.RefreshButton.Click += new System.EventHandler(this.RefreshButton_Click);
+			// 
+			// ActiveSnapshotLabel
+			// 
+			this.ActiveSnapshotLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+			this.ActiveSnapshotLabel.Name = "ActiveSnapshotLabel";
+			this.ActiveSnapshotLabel.Size = new System.Drawing.Size(104, 15);
+			this.ActiveSnapshotLabel.Text = "Viewing snapshot:";
+			// 
+			// toolStripSeparator1
+			// 
+			this.toolStripSeparator1.Name = "toolStripSeparator1";
+			this.toolStripSeparator1.Size = new System.Drawing.Size(6, 23);
+			// 
+			// VisualizerToolbar
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.ToolStrip);
+			this.Name = "VisualizerToolbar";
+			this.Size = new System.Drawing.Size(827, 25);
+			this.ToolStrip.ResumeLayout(false);
+			this.ToolStrip.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ToolStripButton RefreshButton;
+		private System.Windows.Forms.ToolStripLabel ActiveSnapshotLabel;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+		internal System.Windows.Forms.ToolStrip ToolStrip;
+	}
+}
---
+++ SlimTuneUI/VisualizerToolbar.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using UICore;
+
+namespace SlimTuneUI
+{
+	public partial class VisualizerToolbar : UserControl
+	{
+		private Snapshot m_snapshot;
+		private IVisualizer m_visualizer;
+
+		public VisualizerToolbar()
+		{
+			InitializeComponent();
+		}
+
+		private void UpdateSnapshotText()
+		{
+			if(m_snapshot.Id == 0)
+			{
+				ActiveSnapshotLabel.Text = string.Format("Viewing current data as of {0}", DateTime.Now);
+			}
+			else
+			{
+				ActiveSnapshotLabel.Text = string.Format("Viewing snapshot '{0}' from {1}", m_snapshot.Name, m_snapshot.DateTime);
+			}
+		}
+
+		public VisualizerToolbar(Snapshot snapshot, IVisualizer visualizer)
+		{
+			if(snapshot == null)
+				throw new ArgumentNullException("snapshot");
+			if(visualizer == null)
+				throw new ArgumentNullException("visualizer");
+
+			m_snapshot = snapshot;
+			m_visualizer = visualizer;
+
+			InitializeComponent();
+
+			m_visualizer.Refreshed += new EventHandler(m_visualizer_Refreshed);
+			if(m_snapshot.Id == 0 && m_visualizer.SupportsRefresh)
+			{
+				RefreshButton.Visible = true;
+			}
+			else
+			{
+				RefreshButton.Visible = false;
+			}
+
+			UpdateSnapshotText();
+		}
+
+		void m_visualizer_Refreshed(object sender, EventArgs e)
+		{
+			this.Invoke(new UICore.Action(UpdateSnapshotText));
+		}
+
+		private void RefreshButton_Click(object sender, EventArgs e)
+		{
+			m_visualizer.RefreshView();
+		}
+	}
+}
